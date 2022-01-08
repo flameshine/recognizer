2de0f0f53b03b6a324331e2543f46b1f45081159
Reichemann <excellenceissoon@gmail.com>
2022-01-08 12:43:36 +0200

    Initial commit

---
+++ src/main/java/com/flameshine/recognizer/RecognizerApplication.java
@@ -0,0 +1,13 @@
+package com.flameshine.recognizer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecognizerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RecognizerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/flameshine/recognizer/RecognizerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flameshine.recognizer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecognizerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
